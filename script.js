7f5ccce2b83c030efc587b370b7f7c56ead01c65
JFreyra <efreyra@stuy.edu>
2017-03-01 13:03:31 -0500

    animation breaks demos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var img = document.getElementById("img");
+var img = document.getElementById("img1");
 var clear = document.getElementById("clear1");
 
 function getOffset(el) {
@@ -70,3 +70,43 @@ var clearsvg = function(){
 
 clear.addEventListener("click",clearsvg);
 img.addEventListener("mousedown",drawDot);
+
+
+// animation
+
+var img2 = document.getElementById("img1");
+var clear2 = document.getElementById("clear2");
+
+var clearsvg2 = function(){
+    while(img2.lastChild){
+	img2.removeChild(img2.lastChild);
+    };
+};
+
+var drawDot2 = function(radius){
+    var cir = document.getElementsByTagName("circle")[0];
+    var myRad = parseInt(cir.getAttribute("r"));
+    
+    clearsvg2();
+
+    locX = img2.width/2;
+    locY = img2.height/2;
+
+    var c1 = document.createElementNS("http://www.w3.org/2000/svg","circle");
+    
+    c1.setAttribute("cx",locX);
+    c1.setAttribute("cy",locY);
+    c1.setAttribute("r",radius);
+    c1.setAttribute("fill","red");
+    c1.setAttribute("stroke","black");
+
+    img.appendChild(c1);
+};
+
+var animation = function(){
+    var intervalID = window.setInterval(drawDot2,16);
+    
+};
+
+clear.addEventListener("click",clearsvg2);
+img.addEventListener("mousedown",animation);
